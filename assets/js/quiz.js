855f7a4c3d9b733a14afe1750088b0eff59eb251
Mike S Chen <mike.sihong.chen@gmail.com>
2020-06-19 20:31:06 -0400

    updated score and time

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -2,9 +2,16 @@
 
 var questionsList = [
   {
+    question: "What is the correct HTML for adding a background color?",
+    choices: ['<body style="background - color: yellow;">', '<background>yellow</background>', '<body bg="yellow">'],
+    answer: 0
+  },
+  {
     question: "What does CSS stand for?",
     choices: ["Cascading Style Sheets", "Computer Style Sheets", "Creative Style Sheets", "Colorful Style Sheets"],
     answer: 0
   }
+
+  
 
 ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,15 @@ var startBtn = document.getElementById("start-btn");
 
 var scoreRow = document.querySelector(".score-row");
 
-var quizTimeLimit = 15; // Time limit in seconds
+var quizTimeLimit = 20; // Time limit in seconds
 var timerID = document.getElementById("timer");
 
 var scoreID = document.getElementById("score");
 var userScore = 0; // initialize score
+var correctAnswers = 0;
 
-var questionsDiv = document.querySelector(".content");
-var choicesForm = document.querySelector(".multiple-choices");
+var contentDiv = document.querySelector(".content");
+var multipleChoiceForm = document.querySelector(".multiple-choices");
 
 // Function call to start quiz
 startBtn.addEventListener("click", startQuiz);
@@ -18,49 +19,108 @@ startBtn.addEventListener("click", startQuiz);
 function startQuiz() {
   scoreRow.style.display = "flex"; // Unhide score-row container
   startBtn.style.display = "none"; // hide start button
-  showQuestionsAndChoices();
+  contentDiv.style.display = "none";
+  
   countdown();
+  questionsLoop();
+}
+
+// FUNCTION TO LOOP THROUGH QUESTIONS
+function questionsLoop(callback) {
+  var x = 0;
+  loop(x);
+  function loop() {
+if (x < 10) {
+  displayQuestions(x++);
+  userSelection(x++);
+} else {
+  callback();
+}
+  }
+  
 }
 
 // FUNCTION FOR TIMER COUNTDOWN
 function countdown() {
   var timerIntertval = setInterval(function () {
-    quizTimeLimit -= 1;
-    timerID.textContent = "Timer: " + quizTimeLimit + "s";
-
     if (quizTimeLimit <= 10) {
       timer.style.color = "red";
     }
-    if (quizTimeLimit === 0) {
+    if (quizTimeLimit <= 0) {
       clearInterval(timerIntertval);
+      quizTimeLimit = 0;
     }
+    timerID.textContent = "Timer: " + quizTimeLimit + "s";
+    quizTimeLimit -= 1;
   }, 1000);
 }
 
-// FUNCTION TO CHANGE CONTENT div and MULTIPLE CHOICES form
-function showQuestionsAndChoices() {
-  let i = 0;
-  questionsDiv.style.textAlign = 'left';
-  questionsDiv.textContent = `Question ${i + 1}: ${questionsList[i].question}`;
-  
+// FUNCTION TO CREATE CONTENT div and MULTIPLE CHOICES form
+function displayQuestions(questionNumber) {
+  // questionsDiv.style.textAlign = "left";
+  // questionsDiv.textContent = `Question ${questionNumber + 1}: ${questionsList[questionNumber].question}`;
+  let pElement = document.createElement("p");
+  multipleChoiceForm.appendChild(pElement);
+  pElement.style.textAlign = "left";
+  pElement.textContent = `Question ${questionNumber + 1}: ${questionsList[questionNumber].question}`;
 
-  for (let j = 0; j < questionsList[i].choices.length; j++) {
+  for (let i = 0; i < questionsList[questionNumber].choices.length; i++) {
     // create input element and label element for each choice available and append to form tag
     let inputElement = document.createElement("input");
     let labelElement = document.createElement("label");
-    choicesForm.appendChild(inputElement);
-    choicesForm.appendChild(labelElement);
-    
+
+    multipleChoiceForm.appendChild(inputElement);
+    multipleChoiceForm.appendChild(labelElement);
+
     // use radio input type. Hide the radio buttons.
     inputElement.type = "radio";
     inputElement.name = "answer";
-    inputElement.value = j;
-    inputElement.id = j;
+    inputElement.value = i;
+    inputElement.id = i;
     inputElement.style.display = "none";
 
     // display each multiple choice selection using the label element
-    let abcd = "ABCD"; 
-    labelElement.htmlFor = j;
-    labelElement.textContent = abcd[j] + ": " + questionsList[i].choices[j];
+    let abcd = "ABCD";
+    labelElement.htmlFor = i;
+    labelElement.textContent = abcd[i] + ": " + questionsList[questionNumber].choices[i];
+  }
+}
+
+function userSelection(questionNumber) {
+  multipleChoiceForm.addEventListener("click", function (answer) {
+    let userChoice = answer.target.value;
+
+    console.log(userChoice);
+    if (userChoice == questionsList[questionNumber].answer) {
+      multipleChoiceForm.children[2 * userChoice + 2].textContent += " <== Correct!";
+      multipleChoiceForm.children[2 * userChoice + 2].style.color = "green";
+      addScore();
+      setTimeout(clearFormNodes, 1500);
+      return;
+    } else {
+      multipleChoiceForm.children[2 * userChoice + 2].textContent += " <== Incorrect";
+      multipleChoiceForm.children[2 * userChoice + 2].style.color = "red";
+      substractTime();
+      setTimeout(clearFormNodes, 1500);
+      return;
+    }
+  });
+}
+
+// FUNCTION TO ADD TO userScore
+function addScore() {
+  userScore += 10;
+  scoreID.textContent = "Score: " + userScore;
+}
+
+// FUNCTION TO SUBTRACT 5 seconds from quizTimeLimit
+function substractTime() {
+  quizTimeLimit -= 5;
+}
+
+// FUNCTION TO CLEAR FORM CHILDNODES AFTER CHOICE HAS BEEN SELECTED
+function clearFormNodes() {
+  while (multipleChoiceForm.firstChild) {
+    multipleChoiceForm.removeChild(multipleChoiceForm.firstChild);
   }
 }
