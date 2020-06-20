f47d70986c64ddf1acc884bd9eb5545de71cfd0e
Mike S Chen <mike.sihong.chen@gmail.com>
2020-06-20 08:40:38 -0400

    fixed userSelection bug where multiple events were triggered by using stopImmediatePropagation

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -5,6 +5,15 @@ var questionsList = [
     question: "What does CSS stand for?",
     choices: ["Cascading Style Sheets", "Computer Style Sheets", "Creative Style Sheets", "Colorful Style Sheets"],
     answer: 0
+  },
+  {
+    question: "What is the correct HTML for adding a background color?",
+    choices: ['<body style="background - color: yellow;">', "<background>yellow</background>", '<body bg="yellow">'],
+    answer: 0
+  },
+  {
+    question: "Is the sky blue?",
+    choices: ['yes','no', 'the sky is black'],
+    answer: 2
   }
-
-]
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,132 @@
 var startBtn = document.getElementById("start-btn");
+var nextBtn = document.getElementById("next-btn");
 
 var scoreRow = document.querySelector(".score-row");
+var buttonRow = document.querySelector(".button-row");
 
 var quizTimeLimit = 15; // Time limit in seconds
 var timerID = document.getElementById("timer");
 
 var scoreID = document.getElementById("score");
 var userScore = 0; // initialize score
+var answeredCorrectly = 0; // initialize number of correctly answered questions
 
-var questionsDiv = document.querySelector(".content");
-var choicesUL = document.getElementById("multiple-choices");
+var contentDiv = document.querySelector(".content");
+var multipleChoiceUL = document.getElementById("multiple-choices");
+
+var questionNumber = 0;
 
 // Function call to start quiz
 startBtn.addEventListener("click", startQuiz);
 
 // FUNCTION TO START QUIZ
 function startQuiz() {
-  scoreRow.style.display = 'flex'; // Unhide score-row container
-  startBtn.style.display = 'none'; // hide start button
-  showQuestions();
+  scoreRow.style.display = "flex"; // Unhide score-row container
+  startBtn.style.display = "none"; // hide start button
+
   countdown();
+
+  // display first question after Start button is clicked
+  displayQuestion();
+  userSelection();
+
+  // display remaining questions after Next button is clicked
+  nextBtn.addEventListener("click", function() {
+      clearList();
+    if (questionNumber < questionsList.length) {
+      questionNumber++;
+      displayQuestion();
+      userSelection();
+    } else {
+      displayUserScore();
+    }
+  });
 }
 
 // FUNCTION FOR TIMER COUNTDOWN
 function countdown() {
   var timerIntertval = setInterval(function () {
-    quizTimeLimit -= 1;
-    timerID.textContent = "Timer: " + quizTimeLimit + "s";
-
     if (quizTimeLimit <= 10) {
       timer.style.color = "red";
     }
-    if (quizTimeLimit === 0) {
+    if (quizTimeLimit <= 0) {
       clearInterval(timerIntertval);
+      quizTimeLimit = 0;
+      clearList();
+      displayUserScore();
     }
+    timerID.textContent = "Timer: " + quizTimeLimit + "s";
+    quizTimeLimit -= 1;
   }, 1000);
 }
 
-// FUNCTION TO CHANGE CONTENT
-function showQuestions() {
+// FUNCTION TO SHOW QUESTION AND CREATE li TAGS TO SHOW MULTIPLE CHOICES
+function displayQuestion() {
   // console.log(content.children[0]);
   // console.log(questionsList[0].question);
-  let i = 0;
-  questionsDiv.style.textAlign = 'left';
-  questionsDiv.textContent = `Question ${i + 1}: ${questionsList[i].question}`;
-  
+  buttonRow.style.display = "none";
+  contentDiv.children[0].textContent = `Question ${questionNumber + 1}: ${questionsList[questionNumber].question}`;
 
-  for (let j = 0; j < questionsList[i].choices.length; j++) {
+  for (let j = 0; j < questionsList[questionNumber].choices.length; j++) {
     // create a list element for each choice available
     let liElement = document.createElement("li");
-    choicesUL.appendChild(liElement);
+    multipleChoiceUL.appendChild(liElement);
+    liElement.id = j;
+
+    let abcd = "ABCD";
+    multipleChoiceUL.children[j].textContent = abcd[j] + ": " + questionsList[questionNumber].choices[j];
+  }
+}
+
+// FUNCTION TO SELECT FROM LIST OF MULTIPLE CHOICES
+function userSelection() {
+  multipleChoiceUL.addEventListener("click", function (event) {
     
-    let abcd = 'ABCD' 
-    choicesUL.children[j].textContent = abcd[j] + ": " + questionsList[i].choices[j];
+    // stopImmediatePropagation prevents the Eventlistner from firing multiple times on a single click.
+    event.stopImmediatePropagation();
+    
+    let userChoice = event.target.id;
+    console.log(userChoice);
+    if (userChoice == questionsList[questionNumber].answer) {
+      multipleChoiceUL.children[userChoice].textContent += " <== Correct!";
+      multipleChoiceUL.children[userChoice].style.color = "green";
+      addScore();
+      answeredCorrectly += 1;
+      buttonRow.style.display = "flex";
+    } else {
+      multipleChoiceUL.children[userChoice].textContent += " <== Incorrect";
+      multipleChoiceUL.children[userChoice].style.color = "red";
+      substractTime();
+      buttonRow.style.display = "flex";
+    }
+  });
+}
+
+// FUNCTION TO DISPLAY userSore, answeredCorrectly, and ASK USER TO ENTER THEIR NAME
+function displayUserScore() {
+  scoreRow.style.display = "none";
+  buttonRow.style.display = "none";
+  let h2Tag = document.createElement("h2");
+  let pTag = contentDiv.children[0];
+  contentDiv.replaceChild(h2Tag, pTag);
+  contentDiv.firstChild.textContent = `You scored ${userScore} points`;
+}
+
+// FUNCTION TO ADD TO userScore
+function addScore() {
+  userScore += 10; // add 10 points
+  console.log(userScore);
+  scoreID.textContent = "Score: " + userScore;
+}
+
+// FUNCTION TO SUBTRACT 5 seconds FROM quizTimeLimit
+function substractTime() {
+  quizTimeLimit -= 5; // substract 5 seconds
+}
+
+// FUNCTION CLEAR LIST
+function clearList() {
+  while (multipleChoiceUL.firstChild) {
+    multipleChoiceUL.removeChild(multipleChoiceUL.firstChild);
   }
 }
